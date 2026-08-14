700d42393ebbd698e4bbea36bc6b664e707e0304
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-14 22:21:12 +0600

    try catch and status code add

--- server.js
+++ server.js
@@ -12,66 +12,108 @@ const permission = require("./utils/permission");
 const jwt = require("jsonwebtoken");
 
 app.post("/registration", async (req, res) => {
-  let { email, role } = req.body;
-  console.log(email, role);
+  try {
+    let { email, role } = req.body;
 
-  if (!role) {
-    role = "student";
-  }
-  let per;
-  permission.map((item) => {
-    if (item.role == role) {
-      per = item.permission;
+    if (!email) {
+      res.status(400).json({
+        success: false,
+        message: "Email is required",
+      });
     }
-  });
-  console.log(per);
+    if (!role) {
+      role = "student";
+    }
 
-  const user = await new User({
-    email: email,
-    role: role,
-    permission: per,
-  }).save();
+    let per;
+    permission.map((item) => {
+      if (item.role == role) {
+        per = item.permission;
+      }
+    });
 
-  res.send(user);
+    const user = await new User({
+      email: email,
+      role: role,
+      permission: per,
+    }).save();
+
+    res.status(201).json({
+      success: true,
+      user: {
+        user,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Internal server error ${error}`,
+    });
+  }
 });
 
 app.post("/login", async (req, res) => {
   const { email } = req.body;
   const existingUser = await User.findOne({ email: email });
-  console.log(existingUser);
-  const accessToken = jwt.sign(
+  if (!existingUser) {
+    res.status(404).json({
+      success: false,
+      message: "Please register and then login.",
+    });
+  }
+  let accessToken = jwt.sign(
     {
-      _id: existingUser._id,
+      id: existingUser._id,
       email: existingUser.email,
       role: existingUser.role,
     },
     process.env.JWT_SECRET_ACCESS,
     { expiresIn: "1h" },
   );
-  res.json({ accessToken: accessToken });
+
+  res.status(200).json({
+    success: true,
+    message: { accessToken },
+  });
 });
 
-function abc(req, res,next) {
-  let token = req.headers.authorization;
-  console.log(token.split(" ")[1]);
+function abc(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    console.log(token.split(" ")[1]);
 
-
-
-  jwt.verify(
-    token.split(" ")[1],
-    process.env.JWT_SECRET_ACCESS,
-    function (err, decoded) {
-      if (decoded.role == "student") {
-        res.send("You have not access this area");
-      } else {
-        next();
-      }
-    },
-  );
+    jwt.verify(
+      token.split(" ")[1],
+      process.env.JWT_SECRET_ACCESS,
+      function (err, decoded) {
+        console.log(decoded);
+        if (decoded.role == "student") {
+          res.status(403).json("You don't have permission");
+        } else {
+          next();
+        }
+      },
+    );
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Internal server error ${error}`,
+    });
+  }
 }
 
-app.get("/privatedata", abc, async (req, res) => {
-  res.send("All Private Datas");
+app.get("/privateData", abc, async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Amr Facebook Idr Pass *******",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Internal server error ${error}`,
+    });
+  }
 });
 
 mongoose
