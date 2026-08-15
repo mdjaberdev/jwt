f2fc06b7774a249d2f2a98216aa8890424983525
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-16 00:07:32 +0600

    userSchema modify

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,14 +2,22 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  userName: {
+    type: String,
+  },
   email: {
     type: String,
     required: true,
     unique: true,
   },
+  password: {
+    type: String,
+    required: true
+  },
   role: {
     type: String,
     enum: ["student", "teacher", "management"],
+    default: "student"
   },
   permission: {
     type: Array,
--- backend/router/authRouter.js
+++ backend/router/authRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const authMiddleware = require("../middlewares/authMiddleware");
-const registrationController = require("../controllers/registrationController");
-const loginController = require("../controllers/loginController");
-const privateDataController = require("../controllers/privateDataController");
+const { registrationController, loginController } = require("../controllers/authController");
+// const authMiddleware = require("../middlewares/authMiddleware");
+// const registrationController = require("../controllers/registrationController");
+// const loginController = require("../controllers/loginController");
+// const privateDataController = require("../controllers/privateDataController");
 const _ = express.Router();
 
-_.post("/registration", registrationController);
+// _.post("/registration", registrationController);
+// _.post("/login", loginController);
+// _.get("/privateData", authMiddleware, privateDataController);
+
+_.post("/registration", registrationController)
 _.post("/login", loginController);
-_.get("/privateData", authMiddleware, privateDataController);
 
 module.exports = _;
