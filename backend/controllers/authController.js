e4e1ebe7eb5115af84a91cf4904bbc64ba1026de
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-17 21:51:28 +0600

    some validation done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,45 @@
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const strongPasswordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-const registrationController =()=>{
+const registrationController = (req, res) => {
+ try{
+     const { userName, email, password, confirmPassword } = req.body;
 
+  if (!userName || !email || !password || !confirmPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "Please fill all fields",
+    });
+  }
 
-    
-}
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      success: false,
+      message: "Please enter right email",
+    });
+  }
 
-module.exports = {registrationController};
+  if (!strongPasswordRegex.test(password)) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "Password must be 8-20 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.",
+    });
+  }
+
+  if (password == !confirmPassword) {
+     return res.status(400).json({
+       success: false,
+       message:
+         "Password don't match",
+     });
+  }
+ }catch (error){
+    res.status(500).json({
+        success: false
+    })
+ }
+};
+
+module.exports = { registrationController };
